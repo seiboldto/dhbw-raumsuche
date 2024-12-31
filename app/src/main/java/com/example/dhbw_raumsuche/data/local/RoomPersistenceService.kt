c1ddc4081e988233af5aa7facbf0de115bfb87cb
Christian <christiankittler@gmx.de>
2024-12-31 16:30:02 +0100

    🐛Traitor rooms are now being divided up

--- app/src/main/java/com/example/dhbw_raumsuche/data/local/RoomPersistenceService.kt
+++ app/src/main/java/com/example/dhbw_raumsuche/data/local/RoomPersistenceService.kt
@@ -19,12 +19,13 @@ class RoomPersistenceService(private val context: Context) {
     private val eventDao: EventDao by lazy { db.eventDao() }
 
     suspend fun updateDbFromICal(calendars: List<Calendar>) = withContext(
-        Dispatchers.IO) {
+        Dispatchers.IO
+    ) {
         val roomEntities = async {
             calendars
                 .flatMap { it.getComponents<VEvent>(Component.VEVENT) }
                 .mapNotNull { it.location }
-                .mapNotNull { iCalLocationToRoom(it) }
+                .flatMap { iCalLocationToRooms(it) }
                 .toSet()
         }
 
@@ -32,6 +33,7 @@ class RoomPersistenceService(private val context: Context) {
             calendars
                 .flatMap { it.getComponents<VEvent>(Component.VEVENT) }
                 .mapNotNull { iCalEventToEventEntity(it) }
+                .flatten()
                 .toSet()
         }
 
@@ -42,57 +44,66 @@ class RoomPersistenceService(private val context: Context) {
         eventDao.insertEvents(events.toList())
     }
 
-    private fun iCalEventToEventEntity(event: VEvent): EventEntity? {
+    private fun iCalEventToEventEntity(event: VEvent): List<EventEntity>? {
         val id = event.uid ?: return null
         val start = event.startDate ?: return null
         val end = event.endDate ?: return null
         val title = event.summary ?: return null
         val location = event.location ?: return null
-        val roomId = getRoomId(location) ?: return null
+        val roomIds = getRoomIdsToLocation(location)
 
-        return EventEntity(
-            eventId = id.toString(),
-            start = Date(start.date.time),
-            end = Date(end.date.time),
-            title = title.value,
-            roomId = roomId
-        )
+        return roomIds.map {
+            EventEntity(
+                eventId = id.toString(),
+                start = Date(start.date.time),
+                end = Date(end.date.time),
+                title = title.value,
+                roomId = it.key
+            )
+        }.toList()
     }
 
-    private fun getRoomId(location: Location): String? {
-        // The source room names do not follow a specified pattern.
-        // As such, they need to be transformed to [Number][Building].
+    private fun getRoomIdsToLocation(location: Location): Map<String, String> {
+        val roomIds = mutableMapOf<String, String>()
 
-        // "000A ..." or "000 A ..."
-        val numberBuilding = Regex("^(\\d+|\\d+\\.\\d+) ?([A-D]) .*").find(location.value)
-        // "A000 ..." or "A 000 "
-        val buildingNumber = Regex("^([A-D]) ?(\\d+|\\d+\\.\\d+) .*").find(location.value)
+        val parts = location.value.split(",")
 
-        if (numberBuilding != null) {
-            val (number, building) = numberBuilding.destructured
-            return number + building
-        } else if (buildingNumber != null) {
-            val (building, number) = buildingNumber.destructured
-            return number + building
+        val numberBuildingRegex = Regex("^(\\d+|\\d+\\.\\d+) ?([A-D]) .*")
+        val buildingNumberRegex = Regex("^([A-D]) ?(\\d+|\\d+\\.\\d+) .*")
+
+        parts.forEach {
+            val trimmedPart = it.trim()
+            val numberBuilding = numberBuildingRegex.find(trimmedPart)
+            val buildingNumber = buildingNumberRegex.find(trimmedPart)
+
+            if (numberBuilding != null) {
+                val (number, building) = numberBuilding.destructured
+                roomIds[number + building] = trimmedPart
+            } else if (buildingNumber != null) {
+                val (building, number) = buildingNumber.destructured
+                roomIds[number + building] = trimmedPart
+            }
         }
 
-        return null
+        return roomIds
     }
 
-    private fun iCalLocationToRoom(location: Location): RoomEntity? {
-        val id = getRoomId(location) ?: return null
+    private fun iCalLocationToRooms(location: Location): List<RoomEntity> {
+        val idMap = getRoomIdsToLocation(location)
 
-        val building = id[id.length - 1].toString()
-        val number = id.substring(0, id.length - 1)
-        val floor = number[0].toString()
+        return idMap.map { (id, location) ->
+            val building = id[id.length - 1].toString()
+            val number = id.substring(0, id.length - 1)
+            val floor = number[0].toString()
 
-        return RoomEntity(
-            roomId = id,
-            fullName = location.value,
-            building = building,
-            floor = floor,
-            number = number
-        )
+            RoomEntity(
+                roomId = id,
+                fullName = location,
+                building = building,
+                floor = floor,
+                number = number
+            )
+        }
     }
 
     private suspend fun addRoomsToDatabase(roomEntities: List<RoomEntity>) {
