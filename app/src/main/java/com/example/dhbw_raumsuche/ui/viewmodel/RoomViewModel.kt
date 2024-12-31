7bbddaaa99418d66dbd2c1d5b548d8efe6d6fb31
seiboldto <159031527+seiboldto@users.noreply.github.com>
2024-12-31 05:37:01 +0100

    🐛 Remove unused import

--- app/src/main/java/com/example/dhbw_raumsuche/ui/viewmodel/RoomViewModel.kt
+++ app/src/main/java/com/example/dhbw_raumsuche/ui/viewmodel/RoomViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.dhbw_raumsuche.ui.viewmodel
 
 import android.content.Context
-import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
