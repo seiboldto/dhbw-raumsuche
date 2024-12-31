d46fa5479dcb30358554776edd698e480deb90d8
Christian <christiankittler@gmx.de>
2024-12-31 12:14:55 +0100

    🐛The room list is now also filled at the first start

--- app/src/main/java/com/example/dhbw_raumsuche/data/local/dao/RoomDao.kt
+++ app/src/main/java/com/example/dhbw_raumsuche/data/local/dao/RoomDao.kt
@@ -3,7 +3,6 @@ package com.example.dhbw_raumsuche.data.local.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
-import androidx.room.Transaction
 import androidx.room.Upsert
 import com.example.dhbw_raumsuche.data.local.dataclass.RoomWithEvents
 import com.example.dhbw_raumsuche.data.local.entity.RoomEntity
@@ -30,7 +29,6 @@ interface RoomDao {
     fun getRoomsSortByBuilding(): Flow<List<RoomEntity>>
 
     @Query("SELECT * FROM rooms")
-    @Transaction // Ensures that all operations are done in a single transaction
-    suspend fun getRoomsWithEvents(): List<RoomWithEvents>
+    fun getRoomsWithEvents(): Flow<List<RoomWithEvents>>
 
 }
--- app/src/main/java/com/example/dhbw_raumsuche/ical/ICalParser.kt
+++ app/src/main/java/com/example/dhbw_raumsuche/ical/ICalParser.kt
@@ -1,120 +1,37 @@
 package com.example.dhbw_raumsuche.ical
 
-import android.content.Context
-import com.example.dhbw_raumsuche.data.local.RoomsDatabase
-import com.example.dhbw_raumsuche.data.local.dao.EventDao
-import com.example.dhbw_raumsuche.data.local.entity.EventEntity
-import com.example.dhbw_raumsuche.data.local.entity.RoomEntity
 import net.fortuna.ical4j.data.CalendarBuilder
 import net.fortuna.ical4j.model.Calendar
-import net.fortuna.ical4j.model.Component
 import net.fortuna.ical4j.model.TimeZoneRegistry
 import net.fortuna.ical4j.model.TimeZoneRegistryImpl
-import net.fortuna.ical4j.model.component.VEvent
-import net.fortuna.ical4j.model.property.Location
 import net.fortuna.ical4j.util.MapTimeZoneCache
 import java.io.StringReader
-import java.sql.Date
 
-class ICalParser(private val context: Context) {
-    private val db: RoomsDatabase by lazy { RoomsDatabase.getInstance(context) }
-    private val eventDao: EventDao by lazy { db.eventDao() }
+class ICalParser {
 
-    suspend fun parseICal(iCals: List<String>) {
-        // Operate ical4j with minify.
-        // See https://stackoverflow.com/questions/50733209/ical4j-2-2-0-using-grape-throws-java-lang-noclassdeffounderror-javax-cache-con
-        MapTimeZoneCache() // Tell proguard that the class is used
-        System.setProperty(
-            "net.fortuna.ical4j.timezone.cache.impl",
-            "net.fortuna.ical4j.util.MapTimeZoneCache"
-        )
+    companion object {
 
-        val registry: TimeZoneRegistry = TimeZoneRegistryImpl("zoneinfo-outlook-global/")
-        val builder = CalendarBuilder(registry)
+        fun parseICal(iCals: List<String>): List<Calendar> {
+            // Operate ical4j with minify.
+            // See https://stackoverflow.com/questions/50733209/ical4j-2-2-0-using-grape-throws-java-lang-noclassdeffounderror-javax-cache-con
+            MapTimeZoneCache() // Tell proguard that the class is used
+            System.setProperty(
+                "net.fortuna.ical4j.timezone.cache.impl",
+                "net.fortuna.ical4j.util.MapTimeZoneCache"
+            )
 
-        for (iCal in iCals) {
-            val sin = StringReader(cleanMalformedICal(iCal))
-            val calendar: Calendar = builder.build(sin)
-            updateDbFromICal(calendar)
-        }
-    }
+            val registry: TimeZoneRegistry = TimeZoneRegistryImpl("zoneinfo-outlook-global/")
+            val builder = CalendarBuilder(registry)
 
-    private fun cleanMalformedICal(calendarString: String): String {
-        return calendarString.replace("\\n\\s".toRegex(), "")
-    }
-
-    private suspend fun updateDbFromICal(calendar: Calendar) {
-        val events: List<VEvent> = calendar.getComponents(Component.VEVENT)
-        val locations: List<Location> =
-            events.mapNotNull { event -> event.location }.toSet().toList()
-
-        for (location in locations) {
-            val room = iCalLocationToRoom(location) ?: continue
-            addRoomToDatabase(room)
-        }
-
-        for (event in events) {
-            val eventEntity = iCalEventToEventEntity(event)
-            if (eventEntity != null) {
-                eventDao.insertEvent(eventEntity)
+            return iCals.map { iCal ->
+                val sin = StringReader(cleanMalformedICal(iCal))
+                builder.build(sin)
             }
         }
-    }
 
-    private fun iCalEventToEventEntity(event: VEvent): EventEntity? {
-        val id = event.uid ?: return null
-        val start = event.startDate ?: return null
-        val end = event.endDate ?: return null
-        val title = event.summary ?: return null
-        val location = event.location ?: return null
-        val roomId = getRoomId(location) ?: return null
-
-        return EventEntity(
-            eventId = id.toString(),
-            start = Date(start.date.time),
-            end = Date(end.date.time),
-            title = title.value,
-            roomId = roomId
-        )
-    }
-
-    private fun getRoomId(location: Location): String? {
-        // The source room names do not follow a specified pattern.
-        // As such, they need to be transformed to [Number][Building].
-
-        // "000A ..." or "000 A ..."
-        val numberBuilding = Regex("^(\\d+|\\d+\\.\\d+) ?([A-D]) .*").find(location.value)
-        // "A000 ..." or "A 000 "
-        val buildingNumber = Regex("^([A-D]) ?(\\d+|\\d+\\.\\d+) .*").find(location.value)
-
-        if (numberBuilding != null) {
-            val (number, building) = numberBuilding.destructured
-            return number + building
-        } else if (buildingNumber != null) {
-            val (building, number) = buildingNumber.destructured
-            return number + building
+        private fun cleanMalformedICal(calendarString: String): String {
+            return calendarString.replace("\\n\\s".toRegex(), "")
         }
-
-        return null
     }
 
-    private fun iCalLocationToRoom(location: Location): RoomEntity? {
-        val id = getRoomId(location) ?: return null
-
-        val building = id[id.length - 1].toString()
-        val number = id.substring(0, id.length - 1)
-        val floor = number[0].toString()
-
-        return RoomEntity(
-            roomId = id,
-            fullName = location.value,
-            building = building,
-            floor = floor,
-            number = number
-        )
-    }
-
-    private suspend fun addRoomToDatabase(room: RoomEntity) {
-        db.roomDao().insertRoom(room)
-    }
 }
